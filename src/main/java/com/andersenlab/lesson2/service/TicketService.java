1f740d06148af18d61650c22fb9e7b11527310f0
stvjbs <yarashukartem@icloud.com>
2024-06-12 22:46:30 +0200

    feature/lesson4: add NullableWarning annotation, NullChecker, Main classes. refactor Shareable, TicketTest.
    ** Task finished according to task.

---
+++ src/main/java/com/andersenlab/lesson2/service/TicketService.java
@@ -0,0 +1,13 @@
+package com.andersenlab.lesson2.service;
+
+public class TicketService {
+    public static final String ID = "5Gsr";
+    public static final String VEGA_ARENA = "Vega Arena";
+    public static final long EVENT_DATE_TIMESTAMP = 1723240800;
+    public static final double MAX_BACKPACK_WEIGHT = 5.5;
+    public static final String EVENT_CODE = "001";
+    public static final int PRICE_IN_CENTS = 20000;
+
+
+
+}
